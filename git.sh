7626557a58c1c57da0081c403b63aa0d55d90449
textbrowser <textbrowser@gmail.com>
2023-09-22 14:28:07 -0400

    New pages.

---
+++ git.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+# Alexis Megas.
+
+if [ -z ${GIT_A} ];
+then
+    echo "Please export GIT_A."
+    exit 1
+fi
+
+if [ -z ${GIT_T} ];
+then
+    echo "Please export GIT_T."
+    exit 1
+fi
+
+while
+    git pull
+    git add .
+    git commit -m "New pages."
+    git push https://${GIT_A}:${GIT_T}@github.com/textbrowser/\
+	spot-on-shared-pages
+    sleep 300
+do true; done
+
+exit 0
