d39ea6fa2313f2741dac34200b6f78d353bda6ed
textbrowser <textbrowser@gmail.com>
2024-07-11 20:47:59 -0400

    New pages.

--- build_index.sh
+++ build_index.sh
@@ -10,18 +10,22 @@ echo "<body>" >> index.html
 
 for directory in $(ls -p | grep '/')
 do
-    echo "<b>$directory</b>" >> index.html
+    echo -n "<b>" >> index.html
+    echo $directory | tr '[:lower:]' '[:upper:]' >> index.html
+    echo "</b>" >> index.html
     echo "<br><br>" >> index.html
+    echo "<ul>" >> index.html
 
     for file in $(ls $directory*_title 2>/dev/null)
     do
 	link=$(echo $file | sed -e 's/_title$//' | sed -e 's/%/%25/g')
 
-	echo -n "<a href=\"$base/$link\">" >> index.html
+	echo -n "<li><a href=\"$base/$link\">" >> index.html
 	cat $file >> index.html
-	echo "</a><br>" >> index.html
+	echo "</a></li><br>" >> index.html
     done
 
+    echo "</ul>" >> index.html
     echo "<br>" >> index.html
 done
 
