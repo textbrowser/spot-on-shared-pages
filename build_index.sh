79b1c48f1b72f27401b8c1c599d8d72cd86b5e5a
Saturn <saturn@Mac-Mini.local>
2024-07-14 22:10:45 -0400

    New pages.

--- build_index.sh
+++ build_index.sh
@@ -5,28 +5,28 @@ base="https://htmlpreview.github.io/?https://github.com/textbrowser/"
 base=$base"spot-on-shared-pages/blob/main"
 
 rm -f index.html
-echo "<html>" >> index.html
-echo "<body>" >> index.html
+/bin/echo "<html>" >> index.html
+/bin/echo "<body>" >> index.html
 
 for directory in $(ls -p | grep '/')
 do
-    echo -n "<b>" >> index.html
-    echo -n $directory | tr '[:lower:]' '[:upper:]' >> index.html
-    echo "</b>" >> index.html
-    echo "<br>" >> index.html
-    echo "<ul>" >> index.html
+    /bin/echo -n "<b>" >> index.html
+    /bin/echo -n $directory | tr '[:lower:]' '[:upper:]' >> index.html
+    /bin/echo "</b>" >> index.html
+    /bin/echo "<br>" >> index.html
+    /bin/echo "<ul>" >> index.html
 
     for file in $(find $directory -name '*_title' -type f)
     do
-	link=$(echo $file | sed -e 's/_title$//' | sed -e 's/%/%25/g')
+	link=$(/bin/echo $file | sed -e 's/_title$//' | sed -e 's/%/%25/g')
 
-	echo -n "<li><a href=\"$base/$link\">" >> index.html
+	/bin/echo -n "<li><a href=\"$base/$link\">" >> index.html
 	cat $file >> index.html
-	echo "</a></li>" >> index.html
+	/bin/echo "</a></li>" >> index.html
     done
 
-    echo "</ul>" >> index.html
+    /bin/echo "</ul>" >> index.html
 done
 
-echo "</body>" >> index.html
-echo "</html>" >> index.html
+/bin/echo "</body>" >> index.html
+/bin/echo "</html>" >> index.html
