e99d4c8c77bc4fbc57d04410501befd0953fcf80
textbrowser <textbrowser@gmail.com>
2025-02-08 13:30:06 -0500

    New pages.

--- git.sh
+++ git.sh
@@ -13,12 +13,22 @@ then
     exit 1
 fi
 
+cd ~/spot-on-shared-pages
+
+if [ ! $? -eq 0 ]
+then
+    echo "Cannot set spot-on-shared-pages as the current directory."
+    exit 1
+fi
+
 while
     git pull
     git add .
     git commit -m "New pages."
     git push \
 	https://${GIT_A}:${GIT_T}@github.com/textbrowser/spot-on-shared-pages
+    echo "Removing empty files."
+    find . ! -path '*.git*' -empty -type f -exec rm -f {} \; 2>/dev/null
     echo "Preparing index.html."
     ./build_index.sh
     echo "Pausing for one hour."
